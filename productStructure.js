a7709284f886ee49888c897e6f2cf16e0f662bbc
AleLincoln <50921123+AleLincoln@users.noreply.github.com>
2021-03-12 19:57:25 +0000

    Almost done

--- productStructure.js
+++ productStructure.js
@@ -14,5 +14,28 @@ const products = [
 ]
 
 module.exports = () => {
-  // Código aqui
+  const obj = {}
+  const obj2 ={}
+
+  const arrc = products.map((item) => item.split('-')[0])
+  const filteredColors = arrc.filter((item, index) => arrc.indexOf(item) === index)
+
+  const arrs = products.map((item) => item.split('-')[1])
+  const filteredSizes = arrs.filter((item, index) => arrs.indexOf(item) === index)
+
+
+  
+
+
+  filteredColors.forEach((color) => {
+    filteredSizes.forEach((size)=>{
+    obj2[size] = products.filter((item) => item.match(color) && item.match(size)).length
+  })
+
+    obj[color] = obj2
+
+  })
+
+  return obj
+
 }
